7d7d65544818090313d34a2a2ab92cd38f2b4635
Ariel Chait <apchait@gmail.com>
2013-10-07 14:20:20 -0700

    bootstrap installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goride::Application.config.secret_key_base = 'e299b951d7db376fc3a72e8c334fc6d7538417edfd495dbbaa6258a2c78eedad5acc70ecefb73e71928b2e61dd19fa00330bbae086e1e8864905853b49bd3e73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goride::Application.config.session_store :cookie_store, key: '_goride_session'
